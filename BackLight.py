f3b28d8a359b1cbe776ae8e1743e257d0c3d97ce
mwfisher3 <37307043+mwfisher3@users.noreply.github.com>
2020-04-10 12:48:52 -0400

    Update BackLight.py

--- BackLight.py
+++ BackLight.py
@@ -4,21 +4,25 @@ from phue import Bridge
 from time import sleep
 import os
 
-b = Bridge('192.168.1.5')
+# Connect to Hue Bridge via IP
+b = Bridge('192.168.1.5') 
 
-lights = b.lights
-
+# Turn off Raspberry Pi and USB Wifi Dongle LEDs
 os.system("sudo bash -c 'echo 0 > /sys/class/leds/led0/brightness'")
 os.system("sudo bash -c 'echo 0 > /sys/class/leds/led1/brightness'")
 os.system("sudo bash -c 'echo 0 > /sys/class/leds/rt2800usb-phy0::radio/brightness'")
 
+# LR = Living Room Motion Sensor (Hue Sensor Address 2)
 LR = False
 
+# Main Loop
 while True:
     LR = b.get_sensor(2, 'state').get("presence")
     if LR == True:
+	# Turn on Backlight for 5 minutes
 	os.system("sudo bash -c 'echo 0 > /sys/class/backlight/fb_s6d02a1/device/backlight/fb_s6d02a1/bl_power'")
 	sleep(300)
     else:
+	# Turn off Backlight
 	os.system("sudo bash -c 'echo 1 > /sys/class/backlight/fb_s6d02a1/device/backlight/fb_s6d02a1/bl_power'")
 	sleep(1)
